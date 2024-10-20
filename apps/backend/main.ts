935ab5921759fa8e6ce4d78bcd31fdbd18ada1a7
Eduarda Kreimer <eduardakreimer@gmail.com>
2024-10-20 13:57:37 -0300

    fix: main.ts

--- apps/backend/main.ts
+++ apps/backend/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import { AppModule } from './src/app/app.module';
 import * as dotenv from 'dotenv';
 
 dotenv.config({ path: process.cwd() + '/.env' });
